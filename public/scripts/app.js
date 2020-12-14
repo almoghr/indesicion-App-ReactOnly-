cd6353c6a0ce7fe46a71b159a12c244c5cc3849c
almog ram <almoghr36@gmail.com>
2020-12-14 09:31:15 +0200

    initial commit

---
+++ public/scripts/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var template = /*#__PURE__*/React.createElement("h1", null, "indesicion-app");
+var root = document.getElementById("root");
+ReactDOM.render(template, root);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+const template = <h1>indesicion-app</h1>;
+const root = document.getElementById("root");
+
+ReactDOM.render(template, root);
